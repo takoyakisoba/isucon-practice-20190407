a7c098d8cf8b439d25ea376b2a155972bd61c417
waiwai-isucon4 <waiwai-isucon4@example.com>
2019-04-07 09:04:00 +0000

    fix: kataritai.sh

--- kataritai.sh
+++ kataritai.sh
@@ -2,5 +2,5 @@
 
 service php-fpm restart
 echo "" > /var/log/nginx/access.log
-/home/isucon/benchmarker bench
+/home/isucon/env.sh /home/isucon/benchmarker bench
 cat /var/log/nginx/access.log | /usr/local/bin/kataribe -f /opt/kataribe.toml
